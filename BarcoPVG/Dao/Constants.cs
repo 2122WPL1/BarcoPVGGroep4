51d25311c5c187638318a371b3124938a0246224
sWeex720 <99887108+sWeex720@users.noreply.github.com>
2022-02-22 10:17:09 +0100

    connection added

--- BarcoPVG/Dao/Constants.cs
+++ BarcoPVG/Dao/Constants.cs
@@ -3,7 +3,8 @@
     internal class SQLConnection
     {
         //hier komt je connectiestring
-        internal static string CONNECTION_STRING;
-        
+        internal static string CONNECTION_STRING = @"Data Source=DESKTOP-H96UNGE\Vives;Initial Catalog=Barco;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
     }
 }
+
