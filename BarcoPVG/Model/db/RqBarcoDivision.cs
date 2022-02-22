37e3c0ed364c4bb2390dcf14957bf0486d5075ed
Amy Norman <r0900514@student.vives.be>
2022-02-22 10:38:10 +0100

    test

---
+++ BarcoPVG/Model/db/RqBarcoDivision.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BarcoPVG.Model.db
+{
+    public partial class RqBarcoDivision
+    {
+        public string Afkorting { get; set; } = null!;
+        public string? Alias { get; set; }
+        public bool? Actief { get; set; }
+    }
+}
