0c5b9764a36c6242398a3d776a9245db7b317654
Amy Norman <r0900514@student.vives.be>
2022-02-22 11:25:16 +0100

    test

--- BarcoPVG/Dao/Constants.cs
+++ BarcoPVG/Dao/Constants.cs
@@ -4,7 +4,5 @@
     {
         //hier komt je connectiestring
         internal static string CONNECTION_STRING = @"Data Source=LAPTOP-9HK2NMEJ\VIVES;Initial Catalog=Barco;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
-
-
     }
 }
