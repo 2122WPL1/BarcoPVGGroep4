eadf94a3225bd8006e0b7ce19620f9d92c9d2ee8
sWeex720 <99887108+sWeex720@users.noreply.github.com>
2022-02-22 10:21:44 +0100

    delete constant

--- BarcoPVG/Dao/Constants.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BarcoPVG.Dao
-{
-    internal class SQLConnection
-    {
-        //hier komt je connectiestring
-        internal static string CONNECTION_STRING = @"Data Source=DESKTOP-H96UNGE\Vives;Initial Catalog=Barco;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
-    }
-}
-
