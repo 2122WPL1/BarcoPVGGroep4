5519425dd40b7ec0ebd2ce1412c9dec0996fcbd4
sWeex720 <99887108+sWeex720@users.noreply.github.com>
2022-03-08 09:42:52 +0100

    registry changing

--- BarcoPVG/Dao/RegistryConnection.cs
+++ BarcoPVG/Dao/RegistryConnection.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Win32;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Win32;
 
 namespace BarcoPVG.Dao
 {
@@ -15,31 +16,11 @@ namespace BarcoPVG.Dao
         /// <param name="subkeyPath">Relative path to the subkey, starting at the Current User key</param>
         /// <returns></returns>
 
-
         //changing the login when opening the application that the username and password is asked instead of the registry key.
         //Jarne
         [AssemblyInitialize]
         public static void AssemblyInitialize(TestContext testContext)
         {
-            //long setPrompt = -1;
-            //string regEditKey = @"SOFTWARE\Microsoft\Windows\CurrentVersion\InternetSettings\Zones\1";
-
-            //using (RegistryKey regKeys =
-            //RegistryKey.OpenBaseKey(RegistryHive.CurrentUser,
-            //RegistryView.Registry64).OpenSubKey(regEditKey, true))
-            //{
-            //    if (regKeys != null)
-            //    {
-            //        setPrompt = Convert.ToInt64(regKeys.GetValue("1A00"));
-            //        if (setPrompt != 65536)
-            //        {
-            //            regKeys.SetValue("1A00", (object)65536,
-            //            RegistryValueKind.DWord);
-            //            regKeys.Close();
-            //            regKeys.Flush();
-            //        }
-            //    }
-            //}
         }
     }
 }
