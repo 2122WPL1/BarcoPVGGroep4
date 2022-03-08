b9caba02ab86d180cc86100570a39e3832ad931b
Sander Roelens <r0887930@student.vives.be>
2022-03-08 14:41:48 +0100

    gitignore

--- BarcoPVG/Dao/Constants.cs
+++ BarcoPVG/Dao/Constants.cs
@@ -3,7 +3,8 @@
     internal class SQLConnection
     {
         //hier komt je connectiestring
-        internal static string CONNECTION_STRING = @"Data Source=LAPTOP-VOC7F8BU\VIVES;Initial Catalog = Barco; Integrated Security = True; Connect Timeout = 30; Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
-        string d = "qsdfqsdfsd";
+        internal static string CONNECTION_STRING = @"Server=LAPTOP-VOC7F8BU\VIVES;Database=Barco;Trusted_Connection=True;";
+
+
     }
 }
