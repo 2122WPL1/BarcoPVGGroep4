160d99bf5d519ce450a5b96acc96c2fad520fc7f
Amy Norman <r0900514@student.vives.be>
2022-03-11 14:25:39 +0100

    123

--- BarcoPVG/ViewModels/ViewModelMain.cs
+++ BarcoPVG/ViewModels/ViewModelMain.cs
@@ -154,7 +154,7 @@ namespace BarcoPVG.Viewmodels
         //Jarne & Amy
         public void DisplayAddUser()
         {
-            this.ViewModel = new ViewModelDatabaseAddUser();
+            this.ViewModel = new ViewModelDatabaseAddUser(); jhg
         }
         public void DisplayRemoveUser()
         {
