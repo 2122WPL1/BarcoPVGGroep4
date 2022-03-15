78c760ed979ceb992c0c25beaeec505ee02d2117
Amy Norman <r0900514@student.vives.be>
2022-03-15 09:23:54 +0100

    Login aanpassing & errors opgelost

--- BarcoPVG/ViewModels/DatabaseManagement/ViewModelDatabaseAddResource.cs
+++ BarcoPVG/ViewModels/DatabaseManagement/ViewModelDatabaseAddResource.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BarcoPVG.Viewmodels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,8 @@ using System.Threading.Tasks;
 
 namespace BarcoPVG.ViewModels.DatabaseManagement
 {
-    public class ViewModelDatabaseAddResource
+    public class ViewModelDatabaseAddResource : AbstractViewModelCollectionRQ
     {
-
+      
     }
 }
