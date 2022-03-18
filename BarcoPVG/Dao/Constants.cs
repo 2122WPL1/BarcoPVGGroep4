08bf1cb33bc2e2400587b6d2aab527fcd4067a20
Sander Roelens <r0887930@student.vives.be>
2022-03-18 11:45:36 +0100

    gitignore

--- BarcoPVG/Dao/Constants.cs
+++ BarcoPVG/Dao/Constants.cs
@@ -4,9 +4,5 @@
     {
         //hier komt je connectiestring
         internal static string CONNECTION_STRING = @"Server=LAPTOP-VOC7F8BU\VIVES;Database=Barco;Trusted_Connection=True;";
-
-
-
-
     }
 }
