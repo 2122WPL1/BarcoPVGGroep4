79d89d40eba36e62174d065e5a62945bc6460e06
Amy Norman <r0900514@student.vives.be>
2022-03-18 15:22:09 +0100

    databasemanagement aanpassingen user

--- BarcoPVG/Dao/Constants.cs
+++ BarcoPVG/Dao/Constants.cs
@@ -3,9 +3,9 @@
     internal class SQLConnection
     {
         //hier komt je connectiestring
-        internal static string CONNECTION_STRING = @"Server=LAPTOP-VOC7F8BU\VIVES;Database=Barco;Trusted_Connection=True;";
+        internal static string CONNECTION_STRING = @"Server=LAPTOP-9HK2NMEJ\VIVES;Database=Barco;Trusted_Connection=True;";
 
-
+        
 
 
     }
