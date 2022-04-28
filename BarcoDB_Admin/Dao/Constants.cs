029955ae44d536f8687700a6d4645f22367c3e9d
Amy Norman <r0900514@student.vives.be>
2022-04-28 16:17:41 +0200

    0

--- BarcoDB_Admin/Dao/Constants.cs
+++ BarcoDB_Admin/Dao/Constants.cs
@@ -4,9 +4,5 @@
     {
         //hier komt je connectiestring
         internal static string CONNECTION_STRING = @"Server=LAPTOP-9HK2NMEJ\VIVES;Database=Barco;Trusted_Connection=True;";
-
-
-
-
     }
 }
