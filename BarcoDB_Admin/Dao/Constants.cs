1c3f9874868e908345592d923092235aa46ff7a4
sWeex720 <99887108+sWeex720@users.noreply.github.com>
2022-04-29 11:41:30 +0200

    Delete Constants.cs

--- BarcoDB_Admin/Dao/Constants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BarcoDB_Admin.Dao
-{
-    internal class SQLConnection
-    {
-        //hier komt je connectiestring
-        internal static string CONNECTION_STRING = @"Server=LAPTOP-9HK2NMEJ\VIVES;Database=Barco;Trusted_Connection=True;";
-    }
-}
