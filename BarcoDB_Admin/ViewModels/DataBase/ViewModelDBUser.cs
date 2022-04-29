ae7f4a753ac229b5a42544933e66487ca98dcc8c
Amy Norman <r0900514@student.vives.be>
2022-04-29 08:37:07 +0200

    schermen komen nu in de mainwindow van BarcoDB_Admin

---
+++ BarcoDB_Admin/ViewModels/DataBase/ViewModelDBUser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BarcoDB_Admin.Viewmodels;
+
+namespace BarcoDB_Admin.ViewModels.DataBase
+{
+     class ViewModelDBUser : AbstractViewModelCollectionRQ
+    {
+        public ViewModelDBUser() : base()
+        {
+            Load();
+        }
+
+        private void Load()
+        {
+
+        }
+    }
+}
--- BarcoDB_Admin/Views/MainWindow.xaml.cs
+++ BarcoDB_Admin/Views/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BarcoDB_Admin.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,7 +14,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace BarcoDB_Admin
+namespace BarcoDB_Admin.Views
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -22,6 +23,8 @@ namespace BarcoDB_Admin
     {
         public MainWindow()
         {
+            DataContext = new ViewModelMain();
+
             InitializeComponent();
         }
     }
