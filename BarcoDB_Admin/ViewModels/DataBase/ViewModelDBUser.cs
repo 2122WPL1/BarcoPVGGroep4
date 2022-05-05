f7f767e4fe6cb4aa985bfbd33e029b058426e449
Sander Roelens <r0887930@student.vives.be>
2022-05-05 14:05:45 +0200

    Remove User

--- BarcoDB_Admin/ViewModels/DataBase/ViewModelDBUser.cs
+++ BarcoDB_Admin/ViewModels/DataBase/ViewModelDBUser.cs
@@ -4,13 +4,17 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using BarcoDB_Admin.Models.Db;
 using BarcoDB_Admin.Viewmodels;
+using Prism.Commands;
 
 namespace BarcoDB_Admin.ViewModels.DataBase
 {
      class ViewModelDBUser : AbstractViewModelBase
     {
+        public DelegateCommand DeleteUser { get; set; }
+
         private List<Person> _AllUsers;
         private Person _SelectedPerson;
 
@@ -27,12 +31,24 @@ namespace BarcoDB_Admin.ViewModels.DataBase
 
         public ViewModelDBUser() : base()
         {
+            DeleteUser = new DelegateCommand(deleteUserFromDB);
             Load();
         }
 
         public void Load()
         {
             _AllUsers = _dao.GetAllUser();
+        }
+        public void deleteUserFromDB()
+        {
+            if (SelectedUser != null)
+            {
+                _dao.RemoveUser(SelectedUser);
+            }
+            else
+            {
+                MessageBox.Show("Geen gebruiker geselecteerd");
+            }
         }
     }
 }
