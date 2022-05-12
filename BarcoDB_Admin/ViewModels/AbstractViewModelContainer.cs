8ace098fbc4d6a24b218930ef2db02f6d212a991
Eakarach Jansang <eakarach.jansang@student.vives.be>
2022-05-12 21:38:15 +0200

    Update DONE DB Management

--- BarcoDB_Admin/ViewModels/AbstractViewModelContainer.cs
+++ BarcoDB_Admin/ViewModels/AbstractViewModelContainer.cs
@@ -9,53 +9,15 @@ namespace BarcoDB_Admin.ViewModels
 {
     public abstract class AbstractViewModelContainer : AbstractViewModelBase
     {
-        // Jobrequest data container
-        // Only one getter/setter needs to be made for all changes in GUI
-        //protected JR _jr;
-        //protected EUT _eut;
         protected Person _Person;
         protected PlResource _Resource;
         protected RqBarcoDivision _Division;
 
 
-        // EUT's
-        // Does not necessarily need to be linked to JR? We can retrieve the JR ID and add it in DAO
-        //public ObservableCollection<EUT> EUTs { get; set; }
-        //protected EUT _selectedEUT;
-
         public AbstractViewModelContainer()
         {
             //EUTs = new ObservableCollection<EUT>();
         }
-
-        // Getters/Setters
-        //public JR JR
-        //{
-        //    get { return _jr; }
-        //    set
-        //    {
-        //        _jr = value;
-        //        OnpropertyChanged();
-        //    }
-        //}
-        //public EUT Eut
-        //{
-        //    get { return _eut; }
-        //    set
-        //    {
-        //        _eut = value;
-        //        OnpropertyChanged();
-        //    }
-        //}
-        //public EUT SelectedEUT
-        //{
-        //    get { return _selectedEUT; }
-        //    set
-        //    {
-        //        _selectedEUT = value;
-        //        OnpropertyChanged();
-        //    }
-        //}
 
         public Person Person
         {
@@ -65,8 +27,15 @@ namespace BarcoDB_Admin.ViewModels
 
         public RqBarcoDivision Division
         {
-            get { return _Division; }
-            set { _Division = value; OnpropertyChanged(); }
+            get 
+            { 
+                return _Division; 
+            }
+            set 
+            { 
+                _Division = value;
+                OnpropertyChanged(); 
+            }
         }
 
         public PlResource Resource
